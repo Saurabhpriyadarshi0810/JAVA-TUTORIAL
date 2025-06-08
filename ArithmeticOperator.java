b77d25c1de4bbdb09bf425810da1489808c54696
Saurabhpriyadarshi0810 <spriyadarshi831@gmail.com>
2025-06-08 12:44:18 +0530

    completed

--- ArithmeticOperator.java
+++ ArithmeticOperator.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 public class ArithmeticOperator {
     public static void main (String[] args){
-        
+        int a = 7;
+        int b = 8;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
     }
 }
